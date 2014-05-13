9fc87ec8d218e682ccdb44193385b81c6d4d9b06
Taito Horiuchi <taito.horiuchi@gmail.com>
2014-05-13 11:40:36 +0300

    release 0.11

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
 
 setup(
     name='hexagonit.socialbutton',
-    version='0.10.2',
+    version='0.11',
     description="Adds viewlets for embedding codes such as social buttons for Plone.",
     long_description=long_description,
     classifiers=[
